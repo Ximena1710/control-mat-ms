6f0a66bcf9c5d53f1ea0b9868a8801b03d780e85
ximena giraldo <xg.neex@gmail.com>
2024-12-02 21:14:27 -0500

    se anadio cors

--- src/main/java/com/lab/controlmat/config/Config.java
+++ src/main/java/com/lab/controlmat/config/Config.java
@@ -8,17 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class Config implements WebMvcConfigurer {
 
-	 @Bean
-	    public WebMvcConfigurer corsConfigurer() {
-	        return new WebMvcConfigurer() {
-	            @Override
-	            public void addCorsMappings(CorsRegistry registry) {
-	                registry.addMapping("/**")
-	                        .allowedOrigins("http://localhost:3000", "http://localhost", "http://localhost:4200", "https://controlmat-ms.onrender.com")
-	                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-	                        .allowedHeaders("*")
-	                        .allowCredentials(true);
-	            }
-	        };
-	    }
-	}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a todas las rutas
+                .allowedOrigins(
+                    "http://localhost:3000",
+                    "http://localhost",
+                    "http://localhost:4200",
+                    "https://controlmat-ms.onrender.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Todos los encabezados permitidos
+                .allowCredentials(true); // Permite cookies o credenciales
+    }
+}
