6b070062b5b8e035a3ea00c9329fdc7bdfa0b935
ximena giraldo <xg.neex@gmail.com>
2024-12-08 14:39:07 -0500

    se realizaron mejoras

--- src/main/java/com/lab/controlmat/config/Config.java
+++ src/main/java/com/lab/controlmat/config/Config.java
@@ -2,13 +2,14 @@ package com.lab.controlmat.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Component
 public class Config implements WebMvcConfigurer {
 
-    @Override
+	@Bean
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica a todas las rutas
                 .allowedOrigins(
